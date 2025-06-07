13889c5b491d5bdc2733696ade379a7666105a33
fznhakiim <zanziah@gmail.com>
2025-06-07 20:20:30 +0700

    ui register, login, login dan landingg page

--- routes/pengguna_routes.py
+++ routes/pengguna_routes.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 from utils.DB import get_db
 from schema.pengguna_schema import UserCreate, UserLogin
 from controller.pengguna_controller import register_user, login_user
+from schema.pengguna_schema import UserCreate, UserLogin, UserResponse
+from model.user import Pengguna
 
 router = APIRouter(
     prefix="/api",
@@ -22,3 +24,10 @@ def api_login(credentials: UserLogin, db: Session = Depends(get_db)):
     if not result["success"]:
         raise HTTPException(status_code=401, detail=result["message"])
     return result
+
+@router.get("/pengguna/{user_id}", response_model=UserResponse)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(Pengguna).filter(Pengguna.id_pengguna == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="Pengguna tidak ditemukan.")
+    return user
