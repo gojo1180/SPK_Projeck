77fcbfcebf15210a7657c45fce9b1bd9783f4a42
nizham31 <nizhamaufr@gmail.com>
2025-06-14 00:10:46 +0700

    update anuan

--- model/admin.py
+++ model/admin.py
@@ -2,6 +2,7 @@
 from sqlalchemy import Column, Integer, String
 from utils.DB import Base
 
+
 class Admin(Base): 
     __tablename__ = "admin"
     id_admin = Column(Integer, primary_key=True, index=True)
